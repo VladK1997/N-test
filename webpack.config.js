1b3d402a77a3f15a791789f113bad0058d1e668f
vkh <vladkukhlii@gmail.com>
2020-07-20 16:45:20 +0300

    start

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,16 @@ module.exports = {
                 ]
             },
             {
+                test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            },
+            {
                 test:  /\.(jpe?g|png|gif|svg)$/i,
                 loader: 'file-loader',
                 options: {
